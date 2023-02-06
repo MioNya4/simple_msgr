672be08c9d30185691eb995b372434bec6caaf1f
MioNya4 <121242737+MioNya4@users.noreply.github.com>
2023-02-06 22:28:02 +0300

    Update anonimys_msgr.sh

--- anonimys_msgr.sh
+++ anonimys_msgr.sh
@@ -2,25 +2,37 @@
 
 #itoa was here, nya~
 
-mkdir -p /mnt/yandexdisk || sudo mkdir -p /mnt/yandexdisk
-
-if [ -d /mnt/yandexdisk ]
-then
-	echo ok
-else
-        echo 'error permission folder create unable can not' 
-
-	 exit 1
-fi
-
-echo 'u will be asked 4 username which is'
-echo ur acc name here
-echo 'and passwd which is'
-echo ur passwd for apps
-echo 'letsgo'
-mount -t davfs https://webdav.yandex.ru /mnt/yandexdisk
-
 USER='username'
+for var in "$@"
+do
+    if [[ "$var" == "-init" ]]
+    then
+		mkdir -p /mnt/yandexdisk || sudo mkdir -p /mnt/yandexdisk
+		if [ -d /mnt/yandexdisk ]
+		then
+			echo ok
+		else
+				echo 'error permission folder create unable can not' 
+
+			exit 1
+		fi
+
+		echo 'u will be asked 4 username which is'
+		echo "ur acc name (sry this cant be automated afaik)"
+		echo 'and passwd which is'
+		echo "ur passwd"
+		echo 'letsgo'
+		mount -t davfs https://webdav.yandex.ru /mnt/yandexdisk
+	fi
+	
+	if [[ "$var" == "-setusername" ]]
+	then
+		printf "your username for this session: "
+		read USER
+	fi
+done
+
+echo "-------messages-------"
 tail -f /mnt/yandexdisk/chat &
 while [[ 1 ]]
 do
